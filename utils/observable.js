c312a3a5c3385043a4537f6adee9815eb15011a0
Shane Arthur <shane.arthur@gmail.com>
2020-06-28 16:12:22 -0400

    Update observable implementation for interval, fromEvent.

--- utils/observable.js
+++ utils/observable.js
@@ -1,19 +1,3 @@
-class Observable {
-    constructor(subscribe) {
-        this._subscribe = subscribe;
-    }
-
-    subscribe(obs) {
-        const observer = new Observer(obs);
-        observer._unsubscribe = this._subscribe(observer);
-
-        return {
-            unsubscribe: observer.unsubscribe()
-        }
-
-    }
-}
-
 class Observer {
     constructor(handler) {
         this.handler = handler;
@@ -31,7 +15,9 @@ class Observer {
             if (this.handler.error) {
                 this.handler.error(err)
             }
+
             this.unsubscribe();
+
         }
     }
 
@@ -40,38 +26,96 @@ class Observer {
             if (this.handler.complete) {
                 this.handler.complete()
             }
+
             this.unsubscribe();
+
         }
     }
 
     unsubscribe() {
         this.isUnsubscribed = true;
-        if (this._unsubscribe) {
-            this._unsubscribe();
+        if (this.handler._unsubscribe) {
+            this.handler._unsubscribe();
         }
+    }
+
+}
+
+class Observable {
+
+    constructor(setup) {
+
+        this._setup = setup;
+
+    }
+
+    static from(values) {
+        return new Observable(observer => {
+            for (const val of values) {
+                observer.next(val);
+            }
+            observer.complete();
+            return () => console.log('observable.from : unsubbed!');
+        });
+    }
+
+    static interval(timer) {
+
+        let i = 0;
+        return new Observable(observer => {
+
+            const intervalId = setInterval(() => observer.next(++i), timer);
+
+            return () => {
+                clearInterval(intervalId);
+                console.log('Observable.interval: unsubscribed');
+            };
+
+        });
+
+    }
+
+    static fromEvent(element, event) {
+        return new Observable(observer => {
+            const eventHandler = (event) => observer.next(event);
+            element.addEventListener(event, eventHandler, false);
+
+            return () => {
+                element.removeEventListener(eventName, element, false);
+                console.log('Observable.fromEvent: unsubscribed');
+            }
+
+        });
+    }
+
+    subscribe(handler) {
+        const observer = new Observer(handler);
+        observer._unsubscribe = this._setup(observer);
+
+        return {
+            unsubscribe: () => observer.unsubscribe()
+        };
+
     }
 }
 
-Observable.from = function (values) {
-    return new Observable(observer => {
-        for (const value of values) {
-            observer.next(value);
-        }
-        observer.complete();
-        return () => console.log('observable.from : unsubscribed');
-    });
-}
+const handler = {
 
-const numbers$ = Observable.from([0, 1, 2, 3, 4]);
-const subscription = numbers$.subscribe({
-    next(value) {
-        console.log(value);
+    next(val) {
+        console.log(val);
     },
     error(err) {
         console.error(err);
     },
     complete() {
-        console.info('done');
+        console.info('complete');
     }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    const element = document.querySelector('button[name="shanes-button"]');
+
+    const event$ = Observable.fromEvent(element, 'click');
+    const sub = event$.subscribe(handler);
+
 });
-setTimeout(subscription.unsubscribe, 500);
