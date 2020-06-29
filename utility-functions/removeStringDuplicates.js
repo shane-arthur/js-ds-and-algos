ccfd278b6316b686b43bdd955659e4babe6884cf
Shane Arthur <shane.arthur@gmail.com>
2020-06-29 19:59:59 -0400

    Remove string duplicates utility function.

---
+++ utility-functions/removeStringDuplicates.js
@@ -0,0 +1,14 @@
+// Create a function that takes a string and returns a
+// new string with duplicates removed
+
+/*
+ const str = 'This is is a test test string';
+ removeDuplicates(str); // 'This is a test string'
+ */
+
+function removeDuplicates(str) {
+    return [...new Set(str.split(' '))].join(' ');
+}
+
+const str = 'This is is a test test string';
+console.log(removeDuplicates(str)); // 'This is a test string'
