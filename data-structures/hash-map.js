0219bbc9690ac324c701f0629f9c7b16eb0e7b26
Shane Arthur <shane.arthur@gmail.com>
2020-11-30 19:24:27 -0500

    Better hashmap with linear probing.

--- data-structures/hash-map.js
+++ data-structures/hash-map.js
@@ -1,41 +1,68 @@
-function Warning(message) {
-    this.message = message;
-}
-
-function hash(string) {
-    return string
-        .split('')
-        .reduce((a, b) => ((a << 5) + a) + b.charCodeAt(0), 5381)
-}
+// hashMap with linear probing;
+// naive linear probing which could not work the best for certain keys near the end of the hashing algo
 
 function HashMap(size) {
     this.size = size;
-    this.values = new Array(size).fill(null);
-    this.keys = new Array(size).fill(null);
-    this.count = 0;
+    this.values = new Array(this.size).fill(0);
+    this.keys = new Array(this.size).fill(0);
+    this.currentSize = 0;
+}
+
+HashMap.prototype.hash = function (key) {
+
+    if (typeof key !== 'string') {
+        key = key.toString();
+    }
+
+    let val = 0;
+    let total = key.length * 26;
+
+    const num = key.split('').reduce((acc, val) => acc + val.charCodeAt(0) - 97, 0);
+    return Math.floor(Math.round(num / total * 100) / 100 * this.size);
+
 }
 
 HashMap.prototype.set = function (key, value) {
-    if (this.count < this.size) {
-        const hashKey = hash(key);
-        this.keys[hashKey] = hashKey;
-        this.values[hashKey] = value;
-        this.count++;
+    if (this.currentSize >= this.size) {
+        throw new Error('Error HashMap size is full');
+    }
+
+    let hashedIndex = this.hash(key);
+
+    while (true) {
+        if (!this.keys[hashedIndex]) {
+            this.keys[hashedIndex] = key;
+            this.values[hashedIndex] = value;
+            break;
+        } else if (!!this.keys[hashedIndex]) {
+            if (this.keys[hashedIndex] === key) {
+                this.values[hashedIndex] = value;
+                break;
+            } else {
+                hashedIndex++;
+            }
+        }
     }
 }
 
 HashMap.prototype.get = function (key) {
-    const k = hash(key);
-    const hashKey = this.keys[k];
-    return this.values[hashKey];
+    let hashedIndex = this.hash(key);
+    if (!this.keys[hashedIndex]) {
+        throw new Error('Error Key not found ~');
+    }
+
+    while (this.keys[hashedIndex]) {
+        if (this.keys[hashedIndex] === key) {
+            return this.values[hashedIndex];
+        } else {
+            hashedIndex++;
+        }
+    }
 }
 
-
-let map = new HashMap(0);
-map.set('abc', 123) // undefined
-
-try {
-    map.set('abb', 1111);
-} catch (err) {
-    console.log(err);
-}
+const hash = new HashMap(10);
+hash.set('shane', 'cool');
+hash.set('shane', 'cool1');
+hash.set('shane2', 'cool2');
+console.log(hash.get('shane'));
+console.log(hash.get('shane2'));
