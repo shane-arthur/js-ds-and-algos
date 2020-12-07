c875b642e93c65c859a9efbcf36fe18661831962
Shane Arthur <shane.arthur@gmail.com>
2020-12-06 21:08:23 -0500

    Add matrix section.

---
+++ data-structures/arrays/matrix2d.js
@@ -0,0 +1,11 @@
+const matrix = [[1,2,3],[4,5,6],[7,8,9]];
+
+function transpose(matrix){
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = i; j < matrix[0].length; j++){
+            const temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
